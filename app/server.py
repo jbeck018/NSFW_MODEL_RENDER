3cd57a3ebaa52a3f3f3d784b6699aeaf42e94874
Jacob Beck <jacob.beck.018@gmail.com>
2019-08-13 10:09:16 -0400

    update info for render
    updated needed info for render

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1DzxaWNjdgmRcT7lhpBcX9WZ-GK3qeMeX'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Clean', 'Porn', 'Sexy']
 path = Path(__file__).parent
 
 app = Starlette()
