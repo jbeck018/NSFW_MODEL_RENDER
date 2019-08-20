ed84fa2ebd3aa06c0dee10b2bb3bed0ff121d40c
Jacob Beck <jacob.beck.018@gmail.com>
2019-08-20 10:25:18 -0400

    new pikl file
    new file for multi classifier

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1DzxaWNjdgmRcT7lhpBcX9WZ-GK3qeMeX'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1gPx4pKOdhhjHb88YUudEwyIbmWuqjnR4'
 export_file_name = 'export.pkl'
 
 classes = ['Clean', 'Porn', 'Sexy']
