aba63fcb2587a714577c04bdb444986d58ceb0d8
Jacob Beck <jacob.beck.018@gmail.com>
2019-09-04 14:56:51 -0400

    cleaned up predictions

--- app/server.py
+++ app/server.py
@@ -61,7 +61,8 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)
-    return JSONResponse({'result': str(prediction)})
+    pred_clean = prediction[0]
+    return JSONResponse({'result': 'Prediction ' + str(pred_clean) + '. Raw Data: ' + str(prediction)})
 
 
 if __name__ == '__main__':
